59c2dfbac5d50bdafcca616d73e553f669d75558
Ram519 <64475507+Ram519@users.noreply.github.com>
2020-07-09 12:00:19 +0530

    updated mail

--- mail.py
+++ mail.py
@@ -4,10 +4,10 @@ s = smtplib.SMTP('smtp.gmail.com', 587)
 # start TLS for security 
 s.starttls()   
 # Authentication 
-s.login("sender mail", "password") 
+s.login("rameshwarsomvanshi17@gmail.com", "9834157393") 
 # message to be sent 
 message = "Hey Developer,Our website is up."
 # sending the mail 
-s.sendmail("receiver_mail", "sender_mail", message) 
+s.sendmail("rameshwar.somvanshi@gmail.com", "rameshwarsomvanshi17@gmail.com", message) 
 # terminating the session 
 s.quit()
